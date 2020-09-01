aa21ea91add0739e6be36c7f8d329277e60224dd
muhsin <muhsin@alterra.id>
2020-09-01 21:06:27 +0700

    merge model.go and handler.go become item.go

--- core/item/item.go
+++ core/item/item.go
@@ -2,6 +2,19 @@ package item
 
 import "time"
 
+//Item product item that available to rent or sell
+type Item struct {
+	ID          string
+	Name        string
+	Description string
+	Tags        []string
+	CreatedAt   time.Time
+	CreatedBy   string
+	ModifiedAt  time.Time
+	ModifiedBy  string
+	Version     int
+}
+
 //NewItem create new item
 func NewItem(
 	id string,
